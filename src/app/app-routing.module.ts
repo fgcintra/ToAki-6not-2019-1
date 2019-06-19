3e7e95e3254def1e14b4fbbaa8bea0d29b8e1538
daniel <danielmiranda132@yahoo.com>
2019-06-18 21:57:37 -0300

    Alterações finais

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { GeolocalizacaoComponent } from './geolocalizacao/geolocalizacao.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { LeitorQrCodeComponent } from './leitor-qr-code/leitor-qr-code.component';
+import { OficinasComponent } from './oficinas/oficinas.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
 const routes: Routes = [
   { path: '', component: InicioComponent },
-  { path: 'geolocalizacao', component: GeolocalizacaoComponent }
+  { path: 'qrcode', component: LeitorQrCodeComponent },
+  { path: 'geolocalizacao', component: GeolocalizacaoComponent },
+  { path: 'oficinas', component: OficinasComponent},
+  { path: 'perfil', component: PerfilComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,7 +15,10 @@ import { BurgerMenuComponent } from './menu/menu.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { GeolocalizacaoComponent } from './geolocalizacao/geolocalizacao.component';
 import { InicioComponent } from './inicio/inicio.component';
-
+import { LeitorQrCodeComponent } from './leitor-qr-code/leitor-qr-code.component';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import { OficinasComponent } from './oficinas/oficinas.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
 @NgModule({
   declarations: [
@@ -21,13 +27,20 @@ import { InicioComponent } from './inicio/inicio.component';
     BurgerMenuComponent,
     SidenavComponent,
     GeolocalizacaoComponent,
-    InicioComponent
+    InicioComponent,
+    LeitorQrCodeComponent,
+    OficinasComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FlexLayoutModule,
+    FormsModule, 
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MaterialModule,
+    ZXingScannerModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
